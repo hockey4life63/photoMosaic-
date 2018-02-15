491c6d66c2d51b983eca428a6ff05068b67c381d
hockey4life63 <Amelancon68@gmail.com>
2018-02-15 01:29:26 -0800

    first build finished, added progress bars

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const ImageDb = require('./image')
 const average = require('image-average-color')
 const gm = require('gm')
 const sharp = require('sharp');
-const canvas = require('canvas')
-const testImage = new canvas.Image;
+const canvas = require('canvas-prebuilt')
+const ProgressBar = require('progress')
+
 const DEFAULT_VALUES = {
     IMAGESOURCE: './source_images',
     STARTINGVALUE: 0,
@@ -73,34 +74,6 @@ function saveValue(input, option) {
     }
 }
 
-async function fileLoop(ele, index) {
-    console.log('index:', index)
-    const newPromise = () => {
-        return new Promise(resolve => {
-            average('./source_images/' + ele, (err, color) => {
-                if (err) {} else {
-                    const [r, g, b, a] = color
-                    filename = ele
-                    extension = ele.split().splice(-3).join('')
-                    const newImage = new ImageDb({
-                        r,
-                        g,
-                        b,
-                        a,
-                        filename,
-                        extension
-                    })
-                    newImage.save().then(_ => {
-                        console.log('image info saved')
-                        resolve('saved')
-                    }).catch(err => console.log(err))
-                }
-            })
-        })
-    }
-    await newPromise()
-
-}
 
 const changeSettings = _ =>
     inquirer.prompt(settings()).then(answer => {
@@ -115,7 +88,7 @@ const changeSettings = _ =>
         }
     })
 
-const getImageAvg = async (imagePath, index) => {
+const getImageAvg = async(imagePath, index) => {
     return new Promise(resolve => {
         gm('./source_images/' + imagePath).resize(1, 1).toBuffer(function(err, buffer) {
             const r = buffer.readUInt8(buffer.length - 3)
@@ -131,23 +104,32 @@ const getImageAvg = async (imagePath, index) => {
                 extension
             })
             newImage.save().then(_ => {
-                console.log('image info saved', index)
                 resolve('saved')
             }).catch(err => console.log(err))
         })
     })
 }
 
-const tileImage = (thisImage, i, option, ctx)=>{
-    let tileIndex = i
-    let offsetX = (tileIndex % option.sideCount) * option.tileWidth;
-    let offsetY = Math.floor(tileIndex / option.sideCount) * option.tileHeight;
-    let imgFile = fs.readFileSync(thisImage)
-    let img = new canvas.Image
-    img.src = imgFile
-    ctx.drawImage(img, offsetX, offsetY, option.tileWidth, option.tileHeight);
-    
+const tileImage = (thisImage, i, option, ctx) => {
+    let newPromise = new Promise(resolve => {
+        let tileIndex = i
+        let offsetX = (tileIndex % option.sideCount) * option.tileWidth;
+        let offsetY = Math.floor(tileIndex / option.sideCount) * option.tileHeight;
+        let imgFile = fs.readFileSync(thisImage)
+        fs.readFile(thisImage, (err, imgFile) => {
+            let img = new canvas.Image
+            img.onload = _ => {
+                ctx.drawImage(img, offsetX, offsetY, option.tileWidth, option.tileHeight);
+                imgFile = null
+                img = null
+                resolve()
+            }
+            img.src = imgFile
+        })
+    })
+    return newPromise
 }
+
 
 const analyzePhoto = _ => {
     inquirer.prompt([{
@@ -158,81 +140,103 @@ const analyzePhoto = _ => {
         type: 'input',
         name: 'sideCount',
         message: "Enter number of rows of photos you want:",
+    }, {
+        type: 'input',
+        name: 'tileWidth',
+        message: "Enter tile width:",
+    }, {
+        type: 'input',
+        name: 'tileHeight',
+        message: "Enter tile height:",
     }]).then(async option => {
-        option = {
-          filename:"4 - Fyt8Egq.jpg",
-          sideCount:16,
-          tileWidth: 32,
-          tileHeight: 32,
-        }; 
+
+        options.filename === "" ? option.filename = "4 - Fyt8Egq.jpg" : null
+        options.sideCount === "" ? option.sideCount = 128 : option.sideCount = parseInt(option.sideCount)
+        options.tileWidth === "" ? option.tileWidth = 16 : option.tileWidth = parseInt(option.tileWidth)
+        options.tileHeight === "" ? option.tileHeight = 16 : option.tileHeight = parseInt(option.tileHeight)
+
         sharp('./source_images/' + option.filename)
-             //resize to smatch number of iamges to use
+            //resize to smatch number of iamges to use
             .resize(option.sideCount, option.sideCount)
-             //use buffer for pixelGetter
+            //use buffer for pixelGetter
             .raw()
             .toBuffer()
             .then(async pixelData => {
-                    const docs = []
-                    const pixels = []
-                    for (let i = 0; i < pixelData.length; i+=3) {
-                        let r = pixelData[i]
-                        let g = pixelData[i+1]
-                        let b = pixelData[i+2]
-                        pixels.push(`rgb(${r},${g},${b})`)
-                        i%30 === 0?console.log('match colors:', `rgb(${r},${g},${b})`):null
-                        // find closest match in db sqrt((r1 -r2)^2 + (g1 -g2)^2 +(b1 -b2)^2)
-                        await ImageDb.aggregate([{
-                                $project: {
-                                    diff: {
-                                        '$sqrt': {
-                                            $add: [
-                                                { '$pow': [{ '$subtract': [r, '$b'] }, 2] },
-                                                { '$pow': [{ '$subtract': [g, '$g'] }, 2] },
-                                                { '$pow': [{ '$subtract': [b, '$r'] }, 2] }
-                                            ]
-                                        }
+                const docs = []
+                const pixels = []
+                const imageBar = new ProgressBar('Analyzing Image [:bar] :current/' + pixelData.length + 'Pixels', {
+                    complete: '#',
+                    incomplete: '-',
+                    width: 50,
+                    total: pixelData.length
+                })
+                for (let i = 0; i < pixelData.length; i += 3) {
+                    let r = pixelData[i]
+                    let g = pixelData[i + 1]
+                    let b = pixelData[i + 2]
+                    pixels.push(`rgb(${r},${g},${b})`)
+                    // find closest match in db sqrt((r1 -r2)^2 + (g1 -g2)^2 +(b1 -b2)^2)
+                    await ImageDb.aggregate([{
+                            $project: {
+                                diff: {
+                                    '$sqrt': {
+                                        $add: [
+                                            { '$pow': [{ '$subtract': [r, '$b'] }, 2] },
+                                            { '$pow': [{ '$subtract': [g, '$g'] }, 2] },
+                                            { '$pow': [{ '$subtract': [b, '$r'] }, 2] }
+                                        ]
+                                    }
 
-                                    },
-                                    doc: '$$ROOT'
                                 },
-
+                                doc: '$$ROOT'
                             },
-                            { $sort: { diff: 1 } },
-                            { $limit: 1 }
-                        ]).allowDiskUse(true).then(results => {
-                            i%30 === 0?console.log('dc colors',`rgb(${results[0].doc.r},${results[0].doc.g},${results[0].doc.b})`):null
-                            docs.push('./source_images/'+ results[0].doc.filename)
-                            pixels.push(`rgb(${results[0].doc.b},${results[0].doc.g},${results[0].doc.r})`)
-                        })
-                    }
 
-                    console.log('docs length:', docs.length)
-                    const newCanvas = new canvas(option.sideCount*option.tileHeight, option.sideCount*option.tileWidth)
-                    const ctx = newCanvas.getContext('2d')
-                    const secondCanvas = new canvas(option.sideCount, option.sideCount)
-                    const ctxTwo = secondCanvas.getContext('2d')
-                    for (let i = 0; i < option.sideCount; i++) {
-                      for (let k = 0; k < option.sideCount; k++) {
-                        let color = pixels[(i*option.sideCount)+k]
+                        },
+                        { $sort: { diff: 1 } },
+                        { $limit: 1 }
+                    ]).allowDiskUse(true).then(results => {
+                        docs.push('./source_images/' + results[0].doc.filename)
+                        pixels.push(`rgb(${results[0].doc.r},${results[0].doc.g},${results[0].doc.b})`)
+                        imageBar.tick()
+                    })
+                }
+
+                console.log('docs length:', docs.length)
+                const newCanvas = new canvas(option.sideCount * option.tileHeight, option.sideCount * option.tileWidth)
+                const ctx = newCanvas.getContext('2d')
+                const secondCanvas = new canvas(option.sideCount, option.sideCount)
+                const ctxTwo = secondCanvas.getContext('2d')
+                const buildBar = new ProgressBar('Building Image [:bar] :current/' + docs.length + 'Processed', {
+                    complete: '#',
+                    incomplete: '-',
+                    width: 50,
+                    total: docs.length
+                })
+                for (let i = 0; i < option.sideCount; i++) {
+                    for (let k = 0; k < option.sideCount; k++) {
+                        let color = pixels[(i * option.sideCount) + k]
                         ctxTwo.fillStyle = color
-                        ctxTwo.fillRect(k,i,1,1)
-                      }
+                        ctxTwo.fillRect(k, i, 1, 1)
                     }
-                    for (let i = 0; i < docs.length; i++) {
-                      i%10 === 0?console.log('fina;l build loop:', i):null
-                      tileImage(docs[i], i, option, ctx)
-                    }
-                    newCanvas.toBuffer(function(err, buf){
-                      sharp(buf).toFile('mosaic.jpeg', (err, info)=>{
+                }
+                for (let i = 0; i < docs.length; i++) {
+                    let tiling = tileImage(docs[i], i, option, ctx)
+                    await tiling
+                    buildBar.tick()
+                    tiling = null
+                }
+                newCanvas.toBuffer(function(err, buf) {
+                    sharp(buf).toFile('mosaic.jpeg', (err, info) => {
                         console.log(err, info)
-                      })
-                    });
-                    secondCanvas.toBuffer(function(err, buf){
-                      sharp(buf).toFile('pixelImage.jpeg', (err, info)=>{
+                        process.exit()
+
+                    })
+                });
+                secondCanvas.toBuffer(function(err, buf) {
+                    sharp(buf).toFile('pixelImage.jpeg', (err, info) => {
                         console.log(err, info)
-                      })
-                    });
-                
+                    })
+                });
             })
     })
 }
@@ -242,11 +246,19 @@ const buildDatabase = async _ => {
     let fileArr = fs.readdirSync(current_values.IMAGESOURCE)
     //use settings to chose which files to process
     fileArr = fileArr.slice(current_values.STARTINGVALUE, current_values.STARTINGVALUE + current_values.TOTALIMAGES)
+    const fileBar = new ProgressBar('Analyzing Photos :rate/filesPerSecond [:bar] :current/' + fileArr.length + ' Files Processed', {
+        complete: '#',
+        incomplete: '-',
+        width: 25,
+        total: fileArr.length
+    })
     for (let i = 0; i < fileArr.length; i++) {
         //await to prevent freezing and to allow to run in background
         await getImageAvg(fileArr[i], i)
+        fileBar.tick()
     }
     console.log('Finished!')
+    process.exit()
 }
 
 //program intro/main menu
